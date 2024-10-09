430fb1bf2c406604a254479b3dedf52f2a3fe333
WTCHAI <103483111+WTCHAI@users.noreply.github.com>
2024-10-09 13:30:16 +0700

    Done fibonachi zk

--- circom101/fibonachi/README.md
+++ circom101/fibonachi/README.md
@@ -1,70 +1,26 @@
-1. Creating Circuit & compiles 
+1. Bulding circuit
 The process is compile circuit
+
     circom fibonachi.circom --r1cs --wasm --sym --c
-        --r1cs: it generates the file multiplier2.r1cs that contains the R1CS constraint system of the circuit in binary format.
-        --wasm: it generates the directory multiplier2_js that contains the Wasm code (multiplier2.wasm) and other files needed to generate the witness.
-        --sym : it generates the file multiplier2.sym , a symbols file required for debugging or for printing the constraint system in an annotated mode.
-        --c : it generates the directory multiplier2_cpp that contains several files (multiplier2.cpp, multiplier2.dat, and other common files for every compiled program like main.cpp, MakeFile, etc) needed to compile the C code to generate the witness.
 
-2. Generating witness from our equation with exact private input 
-Require Private input to generate inside folder Power_js
+    .r1cs :  getting r1cs stans for The files contains our circuit in binary form & define constaints mathematically
+    .wasm : files contains WebAssembly which used to generate witness for specific the input that we wanted to keep it private while using them to create proof 
 
+2. Generating witness from our equation with exact private input
     node generate_witness.js fibonachi.wasm privateInput.json witness.wtns
 
-3. Generating proof Snark & power of tau
 
-    snarkjs powersoftau new bn128 12 pot12_0000.ptau -v
+3. Trursted Setup 
+    Generating Proving Key & Verification keys to create [] black blocks 
 
-        [DEBUG] snarkJS: Calculating First Challenge Hash
-        [DEBUG] snarkJS: Calculate Initial Hash: tauG1
-        [DEBUG] snarkJS: Calculate Initial Hash: tauG2
-        [DEBUG] snarkJS: Calculate Initial Hash: alphaTauG1
-        [DEBUG] snarkJS: Calculate Initial Hash: betaTauG1
-        [DEBUG] snarkJS: Blank Contribution Hash:
-                        786a02f7 42015903 c6c6fd85 2552d272
-                        912f4740 e1584761 8a86e217 f71f5419
-                        d25e1031 afee5853 13896444 934eb04b
-                        903a685b 1448b755 d56f701a fe9be2ce
-        [INFO]  snarkJS: First Contribution Hash:
-                        9e63a5f6 2b96538d aaed2372 481920d1
-                        a40b9195 9ea38ef9 f5f6a303 3b886516
-                        0710d067 c09d0961 5f928ea5 17bcdf49
-                        ad75abd2 c8340b40 0e3b18e9 68b4ffef
-
-4. Contribute ceremony 
-    snarkjs powersoftau contribute pot12_0000.ptau pot12_0001.ptau --name="First contribution" -v
-        Enter a random text. (Entropy): text 
-        [DEBUG] snarkJS: Calculating First Challenge Hash
-        [DEBUG] snarkJS: Calculate Initial Hash: tauG1
-        [DEBUG] snarkJS: Calculate Initial Hash: tauG2
-        [DEBUG] snarkJS: Calculate Initial Hash: alphaTauG1
-        [DEBUG] snarkJS: Calculate Initial Hash: betaTauG1
-        [DEBUG] snarkJS: processing: tauG1: 0/8191
-        [DEBUG] snarkJS: processing: tauG2: 0/4096
-        [DEBUG] snarkJS: processing: alphaTauG1: 0/4096
-        [DEBUG] snarkJS: processing: betaTauG1: 0/4096
-        [DEBUG] snarkJS: processing: betaTauG2: 0/1
-        [INFO]  snarkJS: Contribution Response Hash imported: 
-                        c3cb82c0 94595c82 9a5384ac 7dd89073
-                        618e561e a849f59f 62fb3f47 b13cfe48
-                        9634acdb df03d141 7d6e9372 8ab3797a
-                        2e55765b ded493db 0584f7cc 7dcaff21
-        [INFO]  snarkJS: Next Challenge Hash: 
-                        489587b7 8b01fdc5 0dd1bd4c 4153e96d
-                        19650b61 38221c6b ba108c4c dce624a2
-                        559377f2 ab6c589f 57391aed 5ca94f24
-                        d641bbda b26a8728 807ac14b 7bcadae7
-
-
-5. Circuit Specific generating .zkey contain proving & verification keys 
-
-    snarkjs powersoftau prepare phase2 pot12_0001.ptau pot12_final.ptau -v
-
-
-    snarkjs groth16 setup Power.r1cs pot12_final.ptau Power_0000.zkey
+        snarkjs powersoftau new bn128 12 pot12_0000.ptau -v
+        snarkjs powersoftau contribute pot12_0000.ptau pot12_0001.ptau --name="First contribution" -v
+        snarkjs powersoftau prepare phase2 pot12_0001.ptau pot12_final.ptau -v
+4. Generating 
+        snarkjs groth16 setup fibonachi.r1cs pot12_final.ptau fibonachi_0000.zkey
 
 6. Contribute Phase two ceremony
-    snarkjs zkey contribute Power_0000.zkey Power_0001.zkey --name="1st Contributor Name" -v
+    snarkjs zkey contribute fibonachi_0000.zkey fibonachi_0001.zkey --name="1st Contributor Name" -v
 
         Enter a random text. (Entropy): text
         [DEBUG] snarkJS: Applying key: H Section: 0/4
@@ -80,18 +36,18 @@ Require Private input to generate inside folder Power_js
                         0a62c320 98a5917d 32f228b4 c69022ea
 
 7. Export verification key 
-    snarkjs zkey export verificationkey Power_0001.zkey verification_key.json
+    snarkjs zkey export verificationkey fibonachi_0001.zkey verification_key.json
 
 8. Generate & Verifying Proof
 
-    snarkjs groth16 prove Power_0001.zkey witness.wtns proof.json public.json
+    snarkjs groth16 prove fibonachi_0001.zkey witness.wtns proof.json public.json
 
     Testing verify 
     snarkjs groth16 verify verification_key.json public.json proof.json
 
 
 9. Generating verifier contract 
-    snarkjs zkey export solidityverifier Power_0001.zkey verifier.sol
+    snarkjs zkey export solidityverifier fibonachi_0001.zkey verifier.sol
 
 
 10. The Verifier has a view function called verifyProof that returns TRUE if and only if the proof and the inputs are valid. To facilitate the call, you can use snarkJS to generate the parameters of the call by typing:
---
+++ circom101/fibonachi/verifier.sol
@@ -0,0 +1,170 @@
+// SPDX-License-Identifier: GPL-3.0
+/*
+    Copyright 2021 0KIMS association.
+
+    This file is generated with [snarkJS](https://github.com/iden3/snarkjs).
+
+    snarkJS is a free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    snarkJS is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+    or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public
+    License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with snarkJS. If not, see <https://www.gnu.org/licenses/>.
+*/
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract Groth16Verifier {
+    // Scalar field size
+    uint256 constant r    = 21888242871839275222246405745257275088548364400416034343698204186575808495617;
+    // Base field size
+    uint256 constant q   = 21888242871839275222246405745257275088696311157297823662689037894645226208583;
+
+    // Verification Key data
+    uint256 constant alphax  = 3151166905743274085209352102216774841624940972531697283506254163840220398192;
+    uint256 constant alphay  = 7872964253731230633995312142954524537906889650261271954682089670506836089355;
+    uint256 constant betax1  = 12124697378402592433077844476983318482498845281514286269137437865729644259439;
+    uint256 constant betax2  = 19339143247473834187276632988818460107022335814942491386718506337914155652242;
+    uint256 constant betay1  = 17685953318913773685035831672607772503763639091964738753977594762862646115364;
+    uint256 constant betay2  = 9247905773350823821793858555593914442172395854071214636741339671252558170401;
+    uint256 constant gammax1 = 11559732032986387107991004021392285783925812861821192530917403151452391805634;
+    uint256 constant gammax2 = 10857046999023057135944570762232829481370756359578518086990519993285655852781;
+    uint256 constant gammay1 = 4082367875863433681332203403145435568316851327593401208105741076214120093531;
+    uint256 constant gammay2 = 8495653923123431417604973247489272438418190587263600148770280649306958101930;
+    uint256 constant deltax1 = 8959468020847033568011373882597233684070391377969714057864897277937416763005;
+    uint256 constant deltax2 = 3810589149180919682918793721393623843110737090602760700630297419637677540098;
+    uint256 constant deltay1 = 5141692416559786532749581508760705217474735409718732447114628655686672094858;
+    uint256 constant deltay2 = 2447946880172459894833960323520098932554003818821280068414002014239750254914;
+
+    
+    uint256 constant IC0x = 17973757643391141043079390065245094265679257167908440333424563948754205368615;
+    uint256 constant IC0y = 14256436094959030335013283150073464033887698610084923809248448315876190665172;
+    
+    uint256 constant IC1x = 17677700905341319119073087344805158910255898240373536033108172960163690671462;
+    uint256 constant IC1y = 11059957360833338264032017870486566285168376579806579122326761071689291693215;
+    
+ 
+    // Memory data
+    uint16 constant pVk = 0;
+    uint16 constant pPairing = 128;
+
+    uint16 constant pLastMem = 896;
+
+    function verifyProof(uint[2] calldata _pA, uint[2][2] calldata _pB, uint[2] calldata _pC, uint[1] calldata _pubSignals) public view returns (bool) {
+        assembly {
+            function checkField(v) {
+                if iszero(lt(v, r)) {
+                    mstore(0, 0)
+                    return(0, 0x20)
+                }
+            }
+            
+            // G1 function to multiply a G1 value(x,y) to value in an address
+            function g1_mulAccC(pR, x, y, s) {
+                let success
+                let mIn := mload(0x40)
+                mstore(mIn, x)
+                mstore(add(mIn, 32), y)
+                mstore(add(mIn, 64), s)
+
+                success := staticcall(sub(gas(), 2000), 7, mIn, 96, mIn, 64)
+
+                if iszero(success) {
+                    mstore(0, 0)
+                    return(0, 0x20)
+                }
+
+                mstore(add(mIn, 64), mload(pR))
+                mstore(add(mIn, 96), mload(add(pR, 32)))
+
+                success := staticcall(sub(gas(), 2000), 6, mIn, 128, pR, 64)
+
+                if iszero(success) {
+                    mstore(0, 0)
+                    return(0, 0x20)
+                }
+            }
+
+            function checkPairing(pA, pB, pC, pubSignals, pMem) -> isOk {
+                let _pPairing := add(pMem, pPairing)
+                let _pVk := add(pMem, pVk)
+
+                mstore(_pVk, IC0x)
+                mstore(add(_pVk, 32), IC0y)
+
+                // Compute the linear combination vk_x
+                
+                g1_mulAccC(_pVk, IC1x, IC1y, calldataload(add(pubSignals, 0)))
+                
+
+                // -A
+                mstore(_pPairing, calldataload(pA))
+                mstore(add(_pPairing, 32), mod(sub(q, calldataload(add(pA, 32))), q))
+
+                // B
+                mstore(add(_pPairing, 64), calldataload(pB))
+                mstore(add(_pPairing, 96), calldataload(add(pB, 32)))
+                mstore(add(_pPairing, 128), calldataload(add(pB, 64)))
+                mstore(add(_pPairing, 160), calldataload(add(pB, 96)))
+
+                // alpha1
+                mstore(add(_pPairing, 192), alphax)
+                mstore(add(_pPairing, 224), alphay)
+
+                // beta2
+                mstore(add(_pPairing, 256), betax1)
+                mstore(add(_pPairing, 288), betax2)
+                mstore(add(_pPairing, 320), betay1)
+                mstore(add(_pPairing, 352), betay2)
+
+                // vk_x
+                mstore(add(_pPairing, 384), mload(add(pMem, pVk)))
+                mstore(add(_pPairing, 416), mload(add(pMem, add(pVk, 32))))
+
+
+                // gamma2
+                mstore(add(_pPairing, 448), gammax1)
+                mstore(add(_pPairing, 480), gammax2)
+                mstore(add(_pPairing, 512), gammay1)
+                mstore(add(_pPairing, 544), gammay2)
+
+                // C
+                mstore(add(_pPairing, 576), calldataload(pC))
+                mstore(add(_pPairing, 608), calldataload(add(pC, 32)))
+
+                // delta2
+                mstore(add(_pPairing, 640), deltax1)
+                mstore(add(_pPairing, 672), deltax2)
+                mstore(add(_pPairing, 704), deltay1)
+                mstore(add(_pPairing, 736), deltay2)
+
+
+                let success := staticcall(sub(gas(), 2000), 8, _pPairing, 768, _pPairing, 0x20)
+
+                isOk := and(success, mload(_pPairing))
+            }
+
+            let pMem := mload(0x40)
+            mstore(0x40, add(pMem, pLastMem))
+
+            // Validate that all evaluations ∈ F
+            
+            checkField(calldataload(add(_pubSignals, 0)))
+            
+            checkField(calldataload(add(_pubSignals, 32)))
+            
+
+            // Validate all evaluations
+            let isValid := checkPairing(_pA, _pB, _pC, _pubSignals, pMem)
+
+            mstore(0, isValid)
+             return(0, 0x20)
+         }
+     }
+ }
