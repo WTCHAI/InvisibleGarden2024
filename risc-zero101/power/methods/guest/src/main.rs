77f561f5e0d0a79c42eda1a03d0411c272a2e7f8
WTCHAI <103483111+WTCHAI@users.noreply.github.com>
2024-10-11 11:08:33 +0700

    Init power zk

--- risc-zero101/power/methods/guest/src/main.rs
+++ risc-zero101/power/methods/guest/src/main.rs
@@ -2,7 +2,9 @@ use risc0_zkvm::guest::env;
 
 fn main() {
     // TODO: Implement your guest code here
-
+    // Prover 
+    // Risc0_zkvm_guest using for reading input data and commit result 
+    // Risc0_build dependency
     // read the input
     let input: u32 = env::read();
 
